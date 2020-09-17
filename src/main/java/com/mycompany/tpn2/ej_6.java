4948d2db7445c0e7567d5969de30ad9371b35a00
ivan millan <ivanmillan36@gmail.comgit>
2020-09-16 21:11:21 -0300

    fin ej 7

---
+++ src/main/java/com/mycompany/tpn2/ej_6.java
@@ -0,0 +1,19 @@
+/*
+6- Del siguiente String “La lluvia en Mendoza es escasa” indique cual es el tamaño
+de la cadena es decir su número de caracteres.
+ */
+package com.mycompany.tpn2;
+
+/**
+ *
+ * @author ivanmillan36
+ */
+public class ej_6 {
+    public static void main(String[] args) {
+        
+        String s = "La lluvia en Mendoza es escasa";
+        
+        System.out.println("La cantidad de caracteres de la oracion es: " + s.length());
+        
+    }
+}
---
+++ src/main/java/com/mycompany/tpn2/ej_7.java
@@ -0,0 +1,34 @@
+/*
+7- Solicite el ingreso de una cadena y determine el tamaño de la misma y cuantas
+vocales tiene en total (recorre el String con charAt)
+ */
+package com.mycompany.tpn2;
+import java.util.Scanner;
+/**
+ *
+ * @author ivanmillan36
+ */
+public class ej_7 {
+    public static void main(String[] args) {
+        
+        String cadena;
+        Scanner sc = new Scanner(System.in);
+        int cantVocales = 0;
+        int [] vocales = {'a', 'e', 'i', 'o', 'u'};
+        
+        System.out.println("Ingrese una cadena: ");
+        cadena = sc.nextLine();
+        
+        System.out.println("El tamaño de la cadena es: " + cadena.length());
+        
+        for (int i=0; i< cadena.length(); i++){
+            for (int j=0; j < vocales.length; j++ ){
+                if (cadena.charAt(i) == vocales[j]){
+                    cantVocales += 1;
+                }
+            }  
+        }
+            
+        System.out.println("La cadena tiene " + cantVocales + " vocales."); 
+    }
+}
