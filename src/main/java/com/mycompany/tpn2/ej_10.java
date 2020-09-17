f3ec918614e1f07d9f7e1338de4b31652c5b3359
ivan millan <ivanmillan36@gmail.comgit>
2020-09-16 21:38:27 -0300

    fij ej 10

--- src/main/java/com/mycompany/tpn2/ej_10.java
+++ src/main/java/com/mycompany/tpn2/ej_10.java
@@ -1,14 +1,34 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+10- Convertir una frase a mayúsculas o minúsculas, que daremos opción a que el
+usuario lo pida y mostraremos el resultado por pantalla.
  */
 package com.mycompany.tpn2;
 
+import java.util.Scanner;
+import javax.swing.JOptionPane;
 /**
  *
  * @author ivanmillan36
  */
 public class ej_10 {
-    
+    public static void main(String[] args) {
+        
+        String cadena;
+        
+        cadena = JOptionPane.showInputDialog("Ingrese una cadena");
+        
+        String[] botones = {"Mayuscula", "Minuscula"};
+	int ventana = JOptionPane.showOptionDialog( null, 
+                                                    "Pulsa un boton:",
+                                                    "Ventana de seleccion", 
+                                                    JOptionPane.DEFAULT_OPTION, 
+                                                    JOptionPane.QUESTION_MESSAGE, null, 
+                                                    botones, botones[0]);
+        if (ventana == 0){
+            JOptionPane.showMessageDialog(null, cadena.toUpperCase());
+        } else {
+            JOptionPane.showMessageDialog(null, cadena.toLowerCase());
+        }
+        
+    }
 }
